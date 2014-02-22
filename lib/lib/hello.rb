a8ef023ab627902991a8efd55c582cf14e4c5252
Carsten Peterson <carsten.peterson@yale.edu>
2014-02-22 01:29:00 -0500

    i don't know what went wrong

--- lib/lib/hello.rb
+++ lib/lib/hello.rb
@@ -1,4 +1,4 @@
-#Default is "World"
-#Author: Carsten Peterson (email)
-name = ARGV.first || "World"
-puts "Hello, #{ARGV.first}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
